b38ff1d4fe42219d5628e16b2e9828bc1b97328b
John Binning <john.binning17@gmail.com>
2017-03-14 14:20:49 -0600

    write test for ball object, touch ball.js, ball-test.js

---
+++ test/ball-test.js
@@ -0,0 +1,62 @@
+var assert = require('chai').assert;
+var Ball = require('../lib/ball')
+
+describe('ball', () => {
+
+it('should be a function', ()=>{
+  assert.isFunction(Ball)
+})
+
+it('should instantiate a new ball', () => {
+  var ball = new Ball();
+  assert.isObject(ball);
+})
+
+it.skip('should have a default x velocity', ()=>{
+  var ball = new Ball();
+  assert.equal(ball.velocityX, 4)
+})
+
+it.skip('should have a default y velocity', ()=>{
+  var ball = new Ball();
+  assert.equal(ball.velocityY, 4)
+})
+
+it.skip('should change X velocity when hitting the side wall', ()=>{
+  var ball = new Ball();
+  assert.equal(ball.velocityX, 4);
+  ballCollideSide();
+  assert.equal(ball.velocityX, -4)
+})
+
+it.skip('should change Y velocity when hitting the top wall', ()=>{
+  var ball = new Ball();
+  assert.equal(ball.velocityY, 4);
+  ballCollideTop();
+  assert.equal(ball.velocityY, -4)
+})
+
+it.skip('should change Y velocity when hitting the paddle', ()=>{
+  var ball = new Ball();
+  assert.equal(ball.velocityY, -4);
+  ballCollidePaddle();
+  assert.equal(ball.velocityY, 4)
+})
+
+
+it.skip('should dissappear when hitting the bottom', ()=> {
+  var ball = new Ball();
+  ballCollideBottom();
+  assert.isNotObject(ball);
+})
+
+})
+
+module.exports = Ball;
+
+
+
+
+  // it('deck creator is function', () => {
+  //   assert.isFunction(Deck);
+  // });

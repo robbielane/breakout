cc9798dda9ea2de5d29766e01d128cc1976ee532
Robbie Lane <lane.robbie@gmail.com>
2017-03-20 23:34:19 -0600

    update webpack to use es2015 preset

--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,19 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.css$/, loader: "style!css" }
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.scss$/, loader: "style!css!sass" }
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.json', '.css']
+    extensions: ['', '.js', '.json', '.scss', 'css']
   }
 };

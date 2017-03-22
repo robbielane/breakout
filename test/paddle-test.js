21924015445362abd30cde43ff0ffb902432fea5
John Binning <john.binning17@gmail.com>
2017-03-21 20:46:07 -0600

    working paddle collision test add

--- test/paddle-test.js
+++ test/paddle-test.js
@@ -6,22 +6,26 @@ const assert = require('chai').assert
 describe('Paddle', () => {
   const canvas = document.createElement('canvas')
   const ctx = canvas.getContext('2d')
-  const world = new World(canvas);
+  let world = new World(canvas);
 
   it('should be a function', () =>{
     assert.isFunction(Paddle)
   })
 
 
-  it.skip('should work', () => {
+  it('should change ball y velocity when hitting the top of the paddle', () => {
     world.ball.vy = -4;
     assert.equal(world.ball.vy, -4);
-    world.ball.x + world.ball.radius == 200;
-    world.ball.x - world.ball.radius == 180;
-    world.ball.y + world.ball.radius == 451;
-    world.paddle.x = 190;
+    world.ball.x = 301;
+    world.ball.y = 441;
     world.paddleCollision();
+    assert.equal(world.ball.x, 301)
+    assert.equal(world.ball.radius, 10)
     assert.equal(world.ball.vy, 4);
+  })
+
+  it('should move with the mouse', () => {
+    
   })
 
 })
